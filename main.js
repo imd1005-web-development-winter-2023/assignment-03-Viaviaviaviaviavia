0356740bd66233410b4244916543f6de546f0b69
Viaviaviaviaviavia <125609420+Viaviaviaviaviavia@users.noreply.github.com>
2023-04-03 00:51:35 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,44 +1,71 @@
-//
-//  JS File
-//  You may remove the code below - it's just boilerplate
-//
 
-//
-// Variables
-//
+const todos = [];
 
-// Constants
-const appID = "app";
-const headingText = "To do. To done. ✅";
+const todoList = document.querySelector(".list");
 
-// Variables
+const todoForm = document.querySelector(".add-todo");
 
-// DOM Elements
-let appContainer = document.getElementById(appID);
+const todoItem = document.querySelector("#todo-list");
 
-//
-// Functions
-//
 
-// Add a heading to the app container
-function inititialise() {
-  // If anything is wrong with the app container then end
-  if (!appContainer) {
-    console.error("Error: Could not find app contianer");
-    return;
-  }
 
-  // Create an h1 and add it to our app
-  const h1 = document.createElement("h1");
-  h1.innerText = headingText;
-  appContainer.appendChild(h1);
+// Handle the event when a user submits the form
+function addTodoItem(e) {
+    // Stop browser default form submission
+    e.preventDefault();
+    // Get the text from the input field
+    const todoListItem = todoItem.value;
+    // Add the user defined pokemon to our array
+    todos.push(todoListItem);
+    // Draw the list of pokemons pokeList
+    renderList(todos, todoList);
+    // Reset the form so that the text field name is cleared
+    todoForm.reset();
+}
 
-  // Init complete
-  console.log("App successfully initialised");
+// Draw the list of items
+// Step 1: remove all of the children in the UL list
+// Step 2: for each entry in the array add the array item to the list
+function renderList(items, itemsList) {
+    // Clear all of the entries in the list listItem
+    while (itemsList.firstChild) {
+        itemsList.removeChild(itemsList.firstChild);
+    }
+
+    // For each item in the list add a list item
+    for (let i = 0; i < items.length; i++) {
+        // Create the list item and add the text
+
+        const todoDeleteButton = document.createElement("button");
+        todoDeleteButton.textContent = "Delete";
+
+        todoDeleteButton.classList.add("todoDeleteButton");
+        todoDeleteButton.dataset.index = i;
+
+        todoDeleteButton.addEventListener("click", deleteTodo);
+
+        itemsList.appendChild(todoDeleteButton);
+
+
+
+        const itemsList = document.createElement("li");
+        itemsList.textContent = items[i];
+        // On the last item in the list, add the annimation class
+        if (i === items.length - 1) {
+            itemsList.classList.add("not-done");
+        }
+        // Add the list item to the list
+        itemsList.appendChild(listItem);
+    }
 }
 
 //
-// Inits & Event Listeners
+// EVENT LISTENERS AND INITIALISION
 //
 
-inititialise();
+// Add the submit form handler
+todoForm.addEventListener("submit", addTodoItem);
+
+
+// Draw the list
+renderList(todos, todoList);
