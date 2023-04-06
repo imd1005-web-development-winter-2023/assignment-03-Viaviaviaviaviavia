d4e766bb995ad4ef1ec619342909938fca3121df
Viaviaviaviaviavia <125609420+Viaviaviaviaviavia@users.noreply.github.com>
2023-04-06 13:41:42 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,71 +1,101 @@
+let todoArray = [];
 
-const todos = [];
+const todoTest = {
+    name: "do this",
+    isDone: false,
+};
 
-const todoList = document.querySelector(".list");
+const todoListElement = document.querySelector(".todo-list");
 
-const todoForm = document.querySelector(".add-todo");
+// todoListElement.classList.add("test");
 
-const todoItem = document.querySelector("#todo-list");
+const newListItem = document.createElement("li");
+
+newListItem.classList.add("not-done");
+
+todoListElement.appendChild(newListItem);
+///////////////////////////////////////
+const imgChecked = document.createElement("img");
+
+imgChecked.src = "./images/sunflower-sticker.png";
+imgChecked.classList.add("finished");
+imgChecked.alt = "complete";
+imgChecked.width = "55";
+imgChecked.height = "55";
+
+newListItem.appendChild(imgChecked);
+///////////////////////////////////////
+const imgUnchecked = document.createElement("img");
+
+imgUnchecked.src = "./images/empty-checkbox.svg";
+imgUnchecked.classList.add("unfinished");
+imgUnchecked.alt = "incomplete";
+imgUnchecked.width = "55";
+imgUnchecked.height = "55";
+
+newListItem.appendChild(imgUnchecked);
+///////////////////////////////////////
+const todoText = document.createElement("p");
+
+todoText.textContent = "test";
+
+newListItem.appendChild(todoText);
+//////////////////////////////////////
+const todoDeleteButton = document.createElement("button");
+
+todoDeleteButton.textContent = "Delete";
+//idk how buttons work yet
+newListItem.appendChild(todoDeleteButton);
+//////////////////////////////////////
 
 
-
-// Handle the event when a user submits the form
-function addTodoItem(e) {
-    // Stop browser default form submission
-    e.preventDefault();
-    // Get the text from the input field
-    const todoListItem = todoItem.value;
-    // Add the user defined pokemon to our array
-    todos.push(todoListItem);
-    // Draw the list of pokemons pokeList
-    renderList(todos, todoList);
-    // Reset the form so that the text field name is cleared
-    todoForm.reset();
-}
-
-// Draw the list of items
-// Step 1: remove all of the children in the UL list
-// Step 2: for each entry in the array add the array item to the list
-function renderList(items, itemsList) {
-    // Clear all of the entries in the list listItem
-    while (itemsList.firstChild) {
-        itemsList.removeChild(itemsList.firstChild);
+function drawList() {
+    while (todoListElement.hasChildNodes()) {
+        todoListElement.removeChild(todoListElement.firstChild);
     }
 
-    // For each item in the list add a list item
-    for (let i = 0; i < items.length; i++) {
-        // Create the list item and add the text
+    for (let i = 0; i < todoArray.length; i++) {
+        const newListItem = document.createElement("li");
 
-        const todoDeleteButton = document.createElement("button");
-        todoDeleteButton.textContent = "Delete";
-
-        todoDeleteButton.classList.add("todoDeleteButton");
-        todoDeleteButton.dataset.index = i;
-
-        todoDeleteButton.addEventListener("click", deleteTodo);
-
-        itemsList.appendChild(todoDeleteButton);
-
-
-
-        const itemsList = document.createElement("li");
-        itemsList.textContent = items[i];
-        // On the last item in the list, add the annimation class
-        if (i === items.length - 1) {
-            itemsList.classList.add("not-done");
+        if (todoArray[i].isDone === true) {
+            newListItem.classList.add("done");
+        } else {
+            newListItem.classList.add("not-done");
         }
-        // Add the list item to the list
-        itemsList.appendChild(listItem);
+
+        todoListElement.appendChild(newListItem);
+        ///////////////////////////////////////
+        const imgChecked = document.createElement("img");
+
+        imgChecked.src = "./images/sunflower-sticker.png";
+        imgChecked.classList.add("finished");
+        imgChecked.alt = "complete";
+        imgChecked.width = "55";
+        imgChecked.height = "55";
+
+        newListItem.appendChild(imgChecked);
+        ///////////////////////////////////////
+        const imgUnchecked = document.createElement("img");
+
+        imgUnchecked.src = "./images/empty-checkbox.svg";
+        imgUnchecked.classList.add("unfinished");
+        imgUnchecked.alt = "incomplete";
+        imgUnchecked.width = "55";
+        imgUnchecked.height = "55";
+
+        newListItem.appendChild(imgUnchecked);
+        ///////////////////////////////////////
+        const todoText = document.createElement("p");
+
+        todoText.textContent = todoArray[i].name;
+
+        newListItem.appendChild(todoText);
+        //////////////////////////////////////
+        const todoDeleteButton = document.createElement("button");
+
+        todoDeleteButton.textContent = "Delete";
+        //idk how buttons work yet
+        newListItem.appendChild(todoDeleteButton);
+        //////////////////////////////////////
     }
 }
-
-//
-// EVENT LISTENERS AND INITIALISION
-//
-
-// Add the submit form handler
-todoForm.addEventListener("submit", addTodoItem);
-
-
-// Draw the list
-renderList(todos, todoList);
